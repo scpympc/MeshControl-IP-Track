0addec1129d051e7b30e3e6077c03919110cb875
sc <scalero@pympc.com>
2025-09-29 16:19:29 +0200

    first commit

---
+++ db.js
@@ -0,0 +1,34 @@
+"use strict";
+var Datastore = null;
+var formatId = null;
+
+module.exports.CreateDB = function(meshserver) {
+    var obj = {};
+    var NEMongo = require(__dirname + '/nemongo.js');
+
+    obj.initFunctions = function () {
+        obj.addLocationHistory = function(nodeid, ip, geo) {
+            return obj.db.insertOne({ type: 'location', nodeid: nodeid, ip: ip, geo: geo, timestamp: new Date() });
+        };
+
+        obj.getLocationHistory = function(nodeid) {
+            return obj.db.find({ type: 'location', nodeid: nodeid }).sort({ timestamp: -1 }).toArray();
+        };
+    };
+
+    if (meshserver.args.mongodb) {
+        // MongoDB support can be added here if needed
+    } else {
+        Datastore = require('@yetzt/nedb');
+        if (obj.dbx == null) {
+            obj.dbx = new Datastore({ filename: meshserver.getConfigFilePath('plugin-ip-track.db'), autoload: true });
+            obj.dbx.persistence.setAutocompactionInterval(40000);
+            obj.dbx.ensureIndex({ fieldName: 'nodeid' });
+        }
+        obj.db = new NEMongo(obj.dbx);
+        formatId = function(id) { return id; };
+        obj.initFunctions();
+    }
+
+    return obj;
+}
---
+++ ip-track.js
@@ -0,0 +1,142 @@
+const http = require('http');
+
+module.exports = function (parent) {
+    var obj = {};
+    obj.parent = parent;
+    obj.meshServer = parent.parent;
+    obj.db = null;
+    var map = null; // Variable to hold the map instance
+
+    // Called when the server is starting
+    obj.server_startup = function () {
+        obj.db = require(__dirname + '/db.js').CreateDB(obj.meshServer);
+    };
+
+    // Called to setup custom API endpoints
+    obj.setupHttpHandlers = function (app) {
+        app.get('/plugin/ip-track/locations', (req, res) => {
+            if (!req.query.nodeid) {
+                res.status(400).send('Missing nodeid');
+                return;
+            }
+            obj.db.getLocationHistory(req.query.nodeid)
+                .then(locations => {
+                    res.json(locations);
+                })
+                .catch(err => {
+                    res.status(500).send(err);
+                });
+        });
+    };
+
+    // Called when the web UI is starting
+    obj.onWebUIStartupEnd = function () {
+        const tab = {
+            tabId: 'ip-track-map',
+            tabTitle: 'Map',
+            tabContent: `
+                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+                <div id="ip-track-map-container" style="height: 100%; width: 100%;"></div>
+            `,
+            onDeviceRefreshEnd: function (device) {
+                const mapContainer = document.getElementById('ip-track-map-container');
+                if (!mapContainer) return;
+
+                // Clear previous map instance if it exists
+                if (map) {
+                    map.remove();
+                    map = null;
+                }
+
+                // Load Leaflet script if not already loaded
+                if (typeof L === 'undefined') {
+                    const script = document.createElement('script');
+                    script.src = 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js';
+                    script.onload = () => loadMap(device._id);
+                    document.head.appendChild(script);
+                } else {
+                    loadMap(device._id);
+                }
+            }
+        };
+        obj.parent.webui.registerPluginTab(tab);
+    };
+
+    function loadMap(nodeid) {
+        const mapContainer = document.getElementById('ip-track-map-container');
+        if (!mapContainer) return;
+
+        fetch(`/plugin/ip-track/locations?nodeid=${nodeid}`)
+            .then(response => response.json())
+            .then(locations => {
+                if (locations && locations.length > 0) {
+                    map = L.map('ip-track-map-container').setView([locations[0].geo.lat, locations[0].geo.lon], 13);
+                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+                    }).addTo(map);
+
+                    const markers = [];
+                    locations.forEach(loc => {
+                        if (loc.geo && loc.geo.lat && loc.geo.lon) {
+                            const marker = L.marker([loc.geo.lat, loc.geo.lon])
+                                .addTo(map)
+                                .bindPopup(`<b>IP:</b> ${loc.ip}<br><b>Time:</b> ${new Date(loc.timestamp).toLocaleString()}`);
+                            markers.push(marker);
+                        }
+                    });
+
+                    if (markers.length > 0) {
+                        const group = new L.featureGroup(markers);
+                        map.fitBounds(group.getBounds().pad(0.1));
+                    }
+                } else {
+                    mapContainer.innerHTML = '<div style="padding: 20px;">No location data available for this device.</div>';
+                }
+            })
+            .catch(err => {
+                console.error('[IP-Track] Error loading location data:', err);
+                mapContainer.innerHTML = '<div style="padding: 20px; color: red;">Error loading location data.</div>';
+            });
+    }
+
+    // Called when an agent connects
+    obj.onAgentConnect = function (agent) {
+        if (agent.ip && agent.dbNodeKey) {
+            getGeoLocation(agent.ip, (err, geoData) => {
+                if (err || geoData.status !== 'success') {
+                    // console.log(`[IP-Track] Could not get geolocation for ${agent.ip}.`);
+                    return;
+                }
+                obj.db.addLocationHistory(agent.dbNodeKey, agent.ip, geoData)
+                    .then(() => { /* console.log(`[IP-Track] Saved location for ${agent.name}`); */ })
+                    .catch((e) => { console.log(`[IP-Track] Error saving location for ${agent.name}:`, e); });
+            });
+        }
+    };
+
+    function getGeoLocation(ip, callback) {
+        // Ignore private IPs
+        const ipParts = ip.split('.').map(p => parseInt(p, 10));
+        if (ipParts[0] === 10 || (ipParts[0] === 172 && ipParts[1] >= 16 && ipParts[1] <= 31) || (ipParts[0] === 192 && ipParts[1] === 168)) {
+            return;
+        }
+
+        const url = `http://ip-api.com/json/${ip}`;
+        http.get(url, (res) => {
+            let data = '';
+            res.on('data', (chunk) => { data += chunk; });
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(data);
+                    callback(null, parsedData);
+                } catch (e) {
+                    callback(e, null);
+                }
+            });
+        }).on('error', (err) => {
+            callback(err, null);
+        });
+    }
+
+    return obj;
+};

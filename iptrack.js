1a52b6625f5232319b88cf26afeb657de4f6afe8
sc <scalero@pympc.com>
2025-10-14 16:37:50 +0200

    chore: Bump version to 2.0.6

--- iptrack.js
+++ iptrack.js
@@ -43,7 +43,7 @@ function plugin(parent) {
     };
 
     obj.onDeviceRefreshEnd = function() {
-        console.log('--- EXECUTING IPTRAK PLUGIN SCRIPT v2.0.5 ---');
+        console.log('--- EXECUTING IPTRAK PLUGIN SCRIPT v2.0.6 ---');
         try {
             console.log('iptrack plugin: onDeviceRefreshEnd');
 
